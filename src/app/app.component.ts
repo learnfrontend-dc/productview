2e8a1a5bbe48cf1ee67f086f4b07e3f283ba9dd1
learnfrontend-dc <learnfrontenddc@gmail.com>
2018-05-13 16:06:46 -0500

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [
+    `.logo {
+      max-height: 30px;
+      padding-bottom: 5px;
+    }`
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  public modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
+
+  public openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 }
