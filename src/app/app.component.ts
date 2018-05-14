24cceef06b1776675977e37e98833b62b3d05ce9
learnfrontend-dc <learnfrontenddc@gmail.com>
2018-05-14 18:25:06 -0500

    Style changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,7 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [
-    `.logo {
-      max-height: 30px;
-      padding-bottom: 5px;
-    }`
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   public modalRef: BsModalRef;
