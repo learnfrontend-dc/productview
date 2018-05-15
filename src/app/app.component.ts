ef62373dcef88eeb81d7f752f7c312667af0e94e
learnfrontend-dc <learnfrontenddc@gmail.com>
2018-05-14 19:23:54 -0500

    iframe url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, TemplateRef } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 export class AppComponent {
   public modalRef: BsModalRef;
   constructor(private modalService: BsModalService) {}
-
+  public ordercartUrl="https://ordercart-dc.herokuapp.com/";
   public openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
