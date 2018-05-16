d1c3f6f4b645b2305e6af19ffa43dafebf588c1c
learnfrontend-dc <learnfrontenddc@gmail.com>
2018-05-15 20:38:40 -0500

    window post message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, TemplateRef } from '@angular/core';
+import { Component, TemplateRef, ViewChild, ElementRef } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { BrowserModule, DomSanitizer } from '@angular/platform-browser'
 
@@ -9,37 +9,40 @@ import { BrowserModule, DomSanitizer } from '@angular/platform-browser'
 })
 export class AppComponent {
   public modalRef: BsModalRef;
+  @ViewChild('ordercart') ordercart : ElementRef;
   public productlist : any[] = [
     {
       productname : 'JBL Flip 4',
       code : 'cat1-0001',
-      price : '18.01',
+      price : 18.01,
       rating : '4'
     }, {
       productname : 'Bose Sound Link',
       code : 'cat1-0010',
-      price : '129.05',
+      price : 129.05,
       rating : '5'
     }, {
       productname : 'AB Portable',
       code : 'cat1-0008',
-      price : '19.78',
+      price : 19.78,
       rating : '3'
     }, {
       productname : 'AE-9 Portable',
       code : 'cat1-0011',
-      price : '299.99',
+      price : 299.99,
       rating : '3'
     }, {
       productname : 'JBL Pulse 3',
       code : 'cat1-0009',
-      price : '23.05',
+      price : 23.05,
       rating : '4'
     }
   ];
-  constructor(private modalService: BsModalService, private sanitizer: DomSanitizer) {}
-  public ordercartUrl=this.sanitizer.bypassSecurityTrustResourceUrl("https://ordercart-dc.herokuapp.com/");
-  public openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template);
+  constructor(private modalService: BsModalService, private sanitizer: DomSanitizer) {
+    
+  }
+  public ordercartUrl=this.sanitizer.bypassSecurityTrustResourceUrl("http://localhost:8081");
+  public addToCart(product) {
+    window.frames[0].postMessage(product, '*');
   }
 }
