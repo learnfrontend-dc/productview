24a9d58a3a3e9a87fcd672386c636d10db0e0783
learnfrontend-dc <learnfrontenddc@gmail.com>
2018-05-15 20:43:20 -0500

    iframeurl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,7 @@ export class AppComponent {
   constructor(private modalService: BsModalService, private sanitizer: DomSanitizer) {
     
   }
-  public ordercartUrl=this.sanitizer.bypassSecurityTrustResourceUrl("http://localhost:8081");
+  public ordercartUrl=this.sanitizer.bypassSecurityTrustResourceUrl("http://orderlist.herokuapp.com/");
   public addToCart(product) {
     window.frames[0].postMessage(product, '*');
   }
